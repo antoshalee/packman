3e33b11e99ddcafdf3bcfd6f72c58f81c872521c
Anton Lee <antoshalee@gmail.com>
2014-11-03 15:38:42 +0300

    Update clojure and clear start screen

--- src/packman/core.clj
+++ src/packman/core.clj
@@ -1,7 +1,10 @@
 (ns packman.core
-  (:gen-class))
+  (:import (jline.console ConsoleReader)
+  (:gen-class)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  (let [cr (ConsoleReader.)]
+  	(.clearScreen cr))
   (println "Hello, World!"))
